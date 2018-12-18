3ab5edcfe7e34a4360e5652afdf22fec4f6e2203
cucco <cucco@codeya.co.jp>
2018-12-18 16:45:52 +0900

    fix: indent

--- src/main/java/com/topcoder/scraper/util/HtmlUtils.java
+++ src/main/java/com/topcoder/scraper/util/HtmlUtils.java
@@ -5,19 +5,19 @@ import com.gargoylesoftware.htmlunit.html.HtmlElement;
 
 public class HtmlUtils {
 
-    public static String getTextContent(HtmlElement element) {
-        if (element != null) {
-            return element.getTextContent().trim();
-        }
-        return null;
+  public static String getTextContent(HtmlElement element) {
+    if (element != null) {
+      return element.getTextContent().trim();
     }
+    return null;
+  }
 
-    public static String getAnchorHref(HtmlElement element) {
-        if (element != null) {
-            return ((HtmlAnchor) element).getHrefAttribute();
-        }
-        return null;
+  public static String getAnchorHref(HtmlElement element) {
+    if (element != null) {
+      return ((HtmlAnchor) element).getHrefAttribute();
     }
+    return null;
+  }
 
   public static String getTextContentWithoutDuplicatedSpaces(HtmlElement element) {
     if (element != null) {

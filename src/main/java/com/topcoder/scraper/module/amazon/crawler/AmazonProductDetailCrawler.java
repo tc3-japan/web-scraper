0db079acd87f7639896507f930f5f3fb146534cf
Luke Dupont <luke_dupont@co-graph.com>
2019-03-07 18:36:27 +0900

    Saved .com's; removed redundant string parsing; removed debug log statement

--- src/main/java/com/topcoder/scraper/module/amazon/crawler/AmazonProductDetailCrawler.java
+++ src/main/java/com/topcoder/scraper/module/amazon/crawler/AmazonProductDetailCrawler.java
@@ -123,12 +123,10 @@ public class AmazonProductDetailCrawler {
     for (String data : categoryInfoList) {
 
       // categoryInfo = [rank] [in] [category path]
-      // in may contain ascii char number 160, so replace it with space
+      // it may contain ascii char number 160, so replace it with space
       String[] categoryInfo = data.replace("\u00A0", " ").split(" ", 3);
-      LOGGER.info(">>>>>>>>>>>>>>>>>>>> " + categoryInfo[0]);
       String rankNumberStr = categoryInfo[0].replaceAll("[^\\d.]", "");
-      // remove possible leading # and comma, then convert to int
-      int rank = Integer.valueOf(rankNumberStr.replace("#", "").replace(",", ""));
+      int rank = Integer.valueOf(rankNumberStr);
 
       // remove See [Tt]op 100 info from category path
       String path = categoryInfo[2];

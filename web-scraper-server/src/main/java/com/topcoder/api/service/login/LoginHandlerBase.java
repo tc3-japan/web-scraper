acf768ff46f41d79f1381be4bd1db3c73c34897b
*twxqRBxtlr <kohata@tc3.co.jp>
2019-05-17 13:41:01 +0900

    Add LoginHandlerBase class

---
+++ web-scraper-server/src/main/java/com/topcoder/api/service/login/LoginHandlerBase.java
@@ -0,0 +1,60 @@
+package com.topcoder.api.service.login;
+
+import java.util.Date;
+import java.util.List;
+
+import com.topcoder.common.dao.ECSiteAccountDAO;
+import com.topcoder.common.dao.UserDAO;
+import com.topcoder.common.model.AuthStatusType;
+import com.topcoder.common.repository.ECSiteAccountRepository;
+import com.topcoder.common.repository.UserRepository;
+
+public abstract class LoginHandlerBase implements LoginHandler {
+
+  protected final ECSiteAccountRepository ecSiteAccountRepository;
+  
+  protected final UserRepository userRepository;
+  
+  public LoginHandlerBase(ECSiteAccountRepository ecSiteAccountRepository, UserRepository userRepository) {
+    this.ecSiteAccountRepository = ecSiteAccountRepository;
+    this.userRepository = userRepository;
+  }
+  
+  public void saveSuccessResult(ECSiteAccountDAO ecSiteAccountDAO) {
+    saveResult(ecSiteAccountDAO, AuthStatusType.SUCCESS, null);
+    
+    UserDAO user = userRepository.findOne(ecSiteAccountDAO.getUserId());
+    if (user == null || AuthStatusType.SUCCESS.equals(user.getTotalECStatus())) {
+      return;
+    }
+    
+    List<ECSiteAccountDAO> accounts = ecSiteAccountRepository.findAllByUserId(ecSiteAccountDAO.getUserId());
+    boolean allAccountsInSuccess = accounts.stream().allMatch(a -> AuthStatusType.SUCCESS.equals(a.getAuthStatus()));
+    if (allAccountsInSuccess) {
+      user.setTotalECStatus(AuthStatusType.SUCCESS);
+      user.setUpdateAt(new Date());
+      userRepository.save(user);
+    }
+  }
+  
+  public void saveFailedResult(ECSiteAccountDAO ecSiteAccountDAO, String message) {
+    saveResult(ecSiteAccountDAO, AuthStatusType.FAILED, message);
+    
+    UserDAO user = userRepository.findOne(ecSiteAccountDAO.getUserId());
+    if (user == null || AuthStatusType.FAILED.equals(user.getTotalECStatus())) {
+      return;
+    }
+    
+    user.setTotalECStatus(AuthStatusType.FAILED);
+    user.setUpdateAt(new Date());
+    userRepository.save(user);
+  }
+  
+  protected void saveResult(ECSiteAccountDAO ecSiteAccountDAO, String status, String message) {
+    ecSiteAccountDAO.setAuthStatus(status);
+    ecSiteAccountDAO.setAuthFailReason(message);
+    ecSiteAccountDAO.setUpdateAt(new Date());
+    ecSiteAccountRepository.save(ecSiteAccountDAO);
+  }
+
+}

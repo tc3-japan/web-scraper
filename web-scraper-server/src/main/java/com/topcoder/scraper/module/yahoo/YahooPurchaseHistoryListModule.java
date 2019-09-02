4dcce2751ed7ba95a3aba432e6b6b5843ce27097
Luke Dupont <luke_dupont@co-graph.com>
2019-09-02 15:53:10 +0900

    Reimplemented Yahoo Scraper

---
+++ web-scraper-server/src/main/java/com/topcoder/scraper/module/yahoo/YahooPurchaseHistoryListModule.java
@@ -0,0 +1,89 @@
+package com.topcoder.scraper.module.yahoo;
+
+import com.topcoder.api.service.login.yahoo.YahooLoginHandler;
+import com.topcoder.common.dao.ECSiteAccountDAO;
+import com.topcoder.common.model.PurchaseHistory;
+import com.topcoder.common.repository.ECSiteAccountRepository;
+import com.topcoder.common.traffic.TrafficWebClient;
+import com.topcoder.common.util.Common;
+import com.topcoder.scraper.module.PurchaseHistoryListModule;
+import com.topcoder.scraper.module.yahoo.crawler.YahooPurchaseHistoryListCrawler;
+import com.topcoder.scraper.module.PurchaseHistoryListCrawlerResult;
+import com.topcoder.scraper.service.PurchaseHistoryService;
+import com.topcoder.scraper.service.WebpageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class YahooPurchaseHistoryListModule extends PurchaseHistoryListModule {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(YahooPurchaseHistoryListModule.class);
+
+  private final PurchaseHistoryService purchaseHistoryService;
+  private final WebpageService webpageService;
+  private final ECSiteAccountRepository ecSiteAccountRepository;
+  private final YahooLoginHandler loginHandler;
+
+  @Autowired
+  public YahooPurchaseHistoryListModule(
+    PurchaseHistoryService purchaseHistoryService,
+    ECSiteAccountRepository ecSiteAccountRepository,
+    WebpageService webpageService,
+    YahooLoginHandler loginHandler) {
+    this.purchaseHistoryService = purchaseHistoryService;
+    this.webpageService = webpageService;
+    this.ecSiteAccountRepository = ecSiteAccountRepository;
+    this.loginHandler = loginHandler;
+  }
+
+  @Override
+  public String getECName() {
+    return "yahoo";
+  }
+
+  @Override
+  public void fetchPurchaseHistoryList() throws IOException {
+    
+    Iterable<ECSiteAccountDAO> accountDAOS = ecSiteAccountRepository.findAllByEcSite(getECName());
+    for (ECSiteAccountDAO ecSiteAccountDAO : accountDAOS) {
+
+      if (ecSiteAccountDAO.getEcUseFlag() != Boolean.TRUE) {
+        LOGGER.info("EC Site [" + ecSiteAccountDAO.getId() + ":" + ecSiteAccountDAO.getEcSite() + "] is not active. Skipped.");
+        continue;
+      }
+      Optional<PurchaseHistory> lastPurchaseHistory = purchaseHistoryService.fetchLast(ecSiteAccountDAO.getId());
+
+      TrafficWebClient webClient = new TrafficWebClient(ecSiteAccountDAO.getUserId(), true);
+      LOGGER.info("web client version = " + webClient.getWebClient().getBrowserVersion());
+      boolean restoreRet = Common.restoreCookies(webClient.getWebClient(), ecSiteAccountDAO);
+      if (!restoreRet) {
+        LOGGER.error("skip ecSite id = " + ecSiteAccountDAO.getId() + ", restore cookies failed");
+        continue;
+      }
+      
+      try {
+        YahooPurchaseHistoryListCrawler crawler = new YahooPurchaseHistoryListCrawler(getECName(), webpageService, ecSiteAccountDAO);
+
+        PurchaseHistoryListCrawlerResult crawlerResult = crawler.fetchPurchaseHistoryList(webClient, lastPurchaseHistory.orElse(null), true);
+        webClient.finishTraffic();
+        List<PurchaseHistory> list = crawlerResult.getPurchaseHistoryList();
+
+        if (list != null && list.size() > 0) {
+          list.forEach(purchaseHistory -> purchaseHistory.setAccountId(Integer.toString(ecSiteAccountDAO.getId())));
+          purchaseHistoryService.save(getECName(), list);
+        }
+        LOGGER.info("succeed fetch purchaseHistory for ecSite id = " + ecSiteAccountDAO.getId());
+      } catch (Exception e) { // here catch all exception and did not throw it
+        this.loginHandler.saveFailedResult(ecSiteAccountDAO, e.getMessage());
+        LOGGER.error("failed to PurchaseHistory for ecSite id = " + ecSiteAccountDAO.getId());
+        e.printStackTrace();
+      }
+    }
+  }
+}
---
+++ web-scraper-server/src/main/java/com/topcoder/scraper/module/yahoo/crawler/YahooProductDetailCrawler.java
@@ -0,0 +1,66 @@
+package com.topcoder.scraper.module.yahoo.crawler;
+
+import com.topcoder.common.model.ProductInfo;
+import com.topcoder.common.traffic.TrafficWebClient;
+import com.topcoder.scraper.module.navpage.NavigableProductDetailPage;
+import com.topcoder.scraper.module.ProductDetailCrawler;
+import com.topcoder.scraper.module.ProductDetailCrawlerResult;
+import com.topcoder.scraper.service.WebpageService;
+import java.io.IOException;
+
+/**
+ * Crawl yahoo product detail page
+ */
+public class YahooProductDetailCrawler extends ProductDetailCrawler {
+
+  public YahooProductDetailCrawler(String siteName, WebpageService webpageService) {
+    super(siteName, webpageService);
+  }
+
+  /**
+   *
+   * Fetch product information
+   * 
+   * @param webClient   the web client
+   * @param productCode the product code
+   * @param saveHtml    true if product html page will be saved
+   * @return ProductDetailCrawlerResultk
+   * @throws IOException
+   */
+
+  @Override
+  public ProductDetailCrawlerResult fetchProductInfo(TrafficWebClient webClient, String productCode, boolean saveHtml)
+      throws IOException {
+        
+    ProductInfo productInfo = new ProductInfo();
+    String htmlPath = "https://store.shopping.yahoo.co.jp/" + productCode;
+    System.out.println(" >>> Requesting Page >>> " + htmlPath);
+    NavigableProductDetailPage detailPage = new NavigableProductDetailPage(htmlPath, webClient, productInfo);
+
+    detailPage.setCode("#abuserpt > p:nth-child(3)");
+    detailPage.setName("div.elTitle > h2:nth-child(1)");
+    detailPage.setDistributor("dt.elStore > a:nth-child(1)");
+    detailPage.setPrice(".elNum");
+
+    //setModelNo(productInfo, null); // TODO: Scrape/find kataban (modelNo)
+    String savedPath = null;
+    if (saveHtml) {
+      savedPath = detailPage.savePage("kojima-product-details", siteName, webpageService);
+    }
+
+    return new ProductDetailCrawlerResult(detailPage.getProductInfo(), savedPath);
+  }
+
+  /**
+   * Search product
+   * 
+   * @param webClient the web client
+   * @param search    word
+   * @return String asin no(product code)
+   * @throws IOException
+   */
+  private String searchProduct(TrafficWebClient webClient, String searchWord) throws IOException {
+    System.out.println("\nPretending to searchProduct! Returning null!"); // TODO: Implement
+    return null; // TODO: Implement
+  }
+}

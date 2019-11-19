4365465693d773cacbc2aca0f0f823ace309d6ce
cucco <cucco@codeya.co.jp>
2019-11-19 16:51:03 +0900

    Ph2-Proj01 : mod : arrange module kojima features

---
+++ web-scraper-server/src/main/java/com/topcoder/scraper/module/ecisolatedmodule/kojima/crawler/KojimaProductDetailCrawlerScriptSupport.java
@@ -0,0 +1,9 @@
+package com.topcoder.scraper.module.ecisolatedmodule.kojima.crawler;
+
+import com.topcoder.scraper.module.ecisolatedmodule.crawler.AbstractProductDetailCrawlerScriptSupport;
+
+/**
+ * Kojima implementation of ProductDetailCrawlerScriptSupport
+ */
+public abstract class KojimaProductDetailCrawlerScriptSupport extends AbstractProductDetailCrawlerScriptSupport {
+}

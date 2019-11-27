9808c8b0115028249a8532e126c9d34650e6ca37
Luke Dupont <luke_dupont@co-graph.com>
2019-11-27 15:37:19 +0900

    Implemented rakuten authentication

---
+++ web-scraper-server/src/main/java/com/topcoder/scraper/module/ecisolatedmodule/rakuten/crawler/RakutenAuthenticationCrawlerResult.java
@@ -0,0 +1,18 @@
+package com.topcoder.scraper.module.ecisolatedmodule.rakuten.crawler;
+
+import com.topcoder.common.model.CodeType;
+import com.topcoder.scraper.module.ecisolatedmodule.crawler.AbstractAuthenticationCrawlerResult;
+
+/**
+ * Result from RakutenAuthenticationCrawler
+ */
+public class RakutenAuthenticationCrawlerResult extends AbstractAuthenticationCrawlerResult {
+
+  public RakutenAuthenticationCrawlerResult(boolean success, String htmlPath) {
+    super(success, htmlPath);
+  }
+
+  public RakutenAuthenticationCrawlerResult(boolean success, String reason, CodeType codeType, String img, boolean needContinue) {
+    super(success, reason, codeType, img, needContinue);
+  }
+}

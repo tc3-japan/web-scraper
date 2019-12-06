033416346eea559cc0c8fe7310e30c01adb21734
cucco <cucco@codeya.co.jp>
2019-12-06 11:36:06 +0900

    Ph2-Proj02 : mod : create search api at kojima modules

--- web-scraper-server/src/main/java/com/topcoder/scraper/lib/navpage/NavigableProductListPage.java
+++ web-scraper-server/src/main/java/com/topcoder/scraper/lib/navpage/NavigableProductListPage.java
@@ -1,10 +1,19 @@
 package com.topcoder.scraper.lib.navpage;
 
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlImageInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import com.topcoder.common.traffic.TrafficWebClient;
+import com.topcoder.common.util.HtmlUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
 
 public class NavigableProductListPage extends NavigablePage {
 
@@ -15,19 +24,56 @@ public class NavigableProductListPage extends NavigablePage {
 		LOGGER.info("[constructor] in");
 	}
 
-	public String scrapeProductCodeFromSearchResult(String searchResultSelector, String productCodeAttribute, String adProductClass, String searchWord) {
+	public void searchProductsUsingForm(String searchUrl, String searchFormName, String searchInputName, String searchButtonSelector, String searchWord) {
+		LOGGER.info("[searchProductsUsingForm] in");
+
+		try {
+			HtmlPage topPage    = this.webClient.getWebClient().getPage(searchUrl);
+			HtmlForm searchForm = topPage.getFormByName(searchFormName);
+
+			HtmlTextInput searchInput = searchForm.getInputByName(searchInputName);
+			searchInput.type(searchWord);
+			HtmlImageInput searchButtonInput = topPage.querySelector(searchButtonSelector);
+
+			this.page = webClient.click(searchButtonInput);
+		} catch (IOException e) {
+			LOGGER.info("[searchProductsUsingForm] failed to get products. page: " + searchUrl + "search word:" + searchWord);
+		}
+	}
+
+	public String scrapeProductCodeFromSearchResultByProductAttrName(String searchResultSelector, String productCodeAttribute, String adProductClass, String searchWord) {
+		LOGGER.info("[scrapeProductCodeFromSearchResultByProductAttrName] in");
 
 		HtmlElement element = this.page.querySelector(searchResultSelector);
 		if (element == null) {
 			return null;
 		}
 		//skip ad product
-		if(element.getAttribute("class").contains(adProductClass)) {
-			LOGGER.info(String.format("Skip ad product with search word = %s", searchWord));
-			return null;
+		if (StringUtils.isNotEmpty(adProductClass)) {
+			if (element.getAttribute("class").contains(adProductClass)) {
+				LOGGER.info(String.format("Skip ad product with search word = %s", searchWord));
+				return null;
+			}
 		}
 		//get asin no
 		String productCode = element.getAttribute(productCodeAttribute);
+		if(productCode == null) {
+			return null;
+		}
+		LOGGER.info(String.format("Product is found with search word = %s, product code is %s", searchWord, productCode));
+		return productCode;
+	}
+
+	public String scrapeProductCodeFromSearchResultByProductUrl(String searchResultSelector, String productCodeRegex, String searchWord) {
+		LOGGER.info("[scrapeProductCodeFromSearchResultByProductUrl] in");
+
+		HtmlAnchor anchor = this.page.querySelector(searchResultSelector);
+		if (anchor == null) {
+			return null;
+		}
+		//get product code
+		String hrefStr     = anchor.getHrefAttribute();
+		String productCode = HtmlUtils.extract1(hrefStr, Pattern.compile(productCodeRegex));
 		if(productCode == null) {
 			return null;
 		}

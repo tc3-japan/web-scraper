6ea386007549ee4afaabfe52aad7a0826ea77210
Tetsuo Shioda <shioda@tc3.co.jp>
2020-04-20 11:29:17 +0900

    Updated configuration insert SQL

--- web-scraper-server/docs/scraper-test_insert_script.sql
+++ web-scraper-server/docs/scraper-test_insert_script.sql
@@ -42,7 +42,7 @@ SET
                 "element": "div:nth-of-type(2) > div:nth-of-type(1) > a.a-link-normal",
                 "full_path": false,
                 "attribute": "href",
-                "regex": "\/gp\/product\/([A-Z0-9]+)\/"
+                "regex": "/gp/product/([A-Z0-9]+)/"
             },
             "product_name": {
                 "element": "div:nth-of-type(2) > div:nth-of-type(1) > a.a-link-normal",
@@ -74,100 +74,183 @@ SET
 }
 ',
 @rakuten_purchase_history_script = '
-import com.topcoder.common.model.ProductInfo;
-print "\\n\\nScraping Rakuten Purchase History\\n\\n"
-def htmlPath = "https://order.my.rakuten.co.jp/?l-id=top_normal_function04&fidomy=1"
-
-setEnableJS(true)
-setPage(htmlPath)
-savePage("test", "rakuten-history")
-
-processPurchaseHistory() {
-	orderList = scrapeDomList "div.oDrListItem"
-	print "orderList loop"
-	//print "ORDER LIST" + orderList
-	int index = 0;
-	processOrders(orderList) { orderNode ->
-		index += 1;
-		//print "looping through orderNode" + orderNode
-		debug "orderNode loop"
-		scrapeOrderNumber(orderNode, "table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(2) > span:nth-child(1)")
-		scrapeOrderDate(orderNode, "table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(1) > div:nth-child(1) > ul:nth-child(1) > li:nth-child(1)")
-
-		//if (!isNew()) { return false; } //PROBLEM! Date changes per order not per item!
-
-		//productList = scrapeDomList orderNode, "table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > div:nth-child(1) > div:nth-child(1)"
-		productList = scrapeDomList orderNode, "table:nth-child(1) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > div"
-		debug "productList: " + productList.size()
-
-		openPage(orderNode, ".purchaseInfo a.detail") { orderDetailPage ->
-
-			distributor = getText orderDetailPage, ".oDrSpecShopInfo .shopName > a"
-			purchaseHistory.totalAmount = getText orderDetailPage, ".priceCalc .netTot"
-
-			productList = scrapeDomList(orderDetailPage, ".oDrSpecPurchaseInfo tr")
-			processProducts(productList) { productNode ->
-
-				String prodInfo = getText productNode, ".prodInfo"
-				if (!prodInfo?.trim())  { // skip if prodInfo is null or empty
-					return
-				}
-				scrapeProductCodeFromAnchor productNode, ".prodInfo .prodName a", ".+item.rakuten.co.jp/([^/]+/[^/]+)/.*"
-				scrapeProductName productNode, ".prodInfo .prodName a"
-				scrapeUnitPrice productNode, ".widthPrice"
-				scrapeProductQuantity productNode, ".widthQuantity"
-				productInfo.distributor = distributor
-			}
-		}
-	}
-}
-',
-@yahoo_purchase_history_script = '
-setEnableJS(true)
-setPage("https://odhistory.shopping.yahoo.co.jp/cgi-bin/history-list?sc_i=shp_pc_top_MHD_order_history")
-savePage("test", "yahoo")
-
-processPurchaseHistory() {
-    // scrape order dom node list
-    orderList = scrapeDomList(".elMain > ul > li")
-
-    // loop each order
-    processOrders(orderList) { orderNode ->
-        // scrape order details
-        //#ordhist > div.elMain > ul > li:nth-child(1) > div.elItem > ul > li > ul.elSummary > li.elOrder > dl > dd
-        scrapeOrderNumber orderNode, "div.elItem > ul > li > ul.elSummary > li.elOrder > dl > dd"
-
-        //#ordhist > div.elMain > ul > li:nth-child(1) > div.elOrderHeader > p.elDate > span
-        scrapeOrderDate   orderNode, "div.elOrderHeader > p.elDate > span"
-
-        //if (!isNew()) { return false; }
-
-        //#ordhist > div.elMain > ul > li:nth-child(1) > div.elItem > ul > li > ul.elButton > li.elDetail > a
-        openPage(orderNode, "div.elItem > ul > li > ul.elButton > li.elDetail > a") { orderDetailPage ->
-
-            //#orddtl > div.elItem > ul > li
-            productList = scrapeDomList(orderDetailPage, "#orddtl > div.elItem > ul > li")
-            processProducts(productList) { productNode ->
-
-                // get product code from the URL like https://store.shopping.yahoo.co.jp/bookfan/BK-481540657X.html
-                //#orddtl > div.elItem > ul > li > div > ul > li > form > input[name=".autodone"]
-                scrapeProductCodeFromInput productNode, "div > ul > li > form > input[name=\\".autodone\\"]", "https:\\\\/\\\\/.*?\\\\/(.*)\\\\.html"
-
-                //#orddtl > div.elItem > ul > li > div > dl > dd.elName > a > span
-                scrapeProductName          productNode, "div > dl > dd.elName > a > span"
-
-                //#orddtl > div.elItem > ul > li > div > dl > dd.elInfo > span.elPrice
-                scrapeUnitPrice            productNode, "div > dl > dd.elInfo > span.elPrice"
-
-                //#orddtl > div.elItem > ul > li > div > dl > dd.elInfo > span.elNum
-                scrapeProductQuantity      productNode, "div > dl > dd.elInfo > span.elNum"
-
-                // CAUTION: At yahoo distributer is out of product list, it''s common for products.
-                //#buystr > div.elItem > ul.elStore > li.elName > a
-                scrapeProductDistributor   orderDetailPage, "#buystr > div.elItem > ul.elStore > li.elName > a"
+{
+    "url": "https://order.my.rakuten.co.jp/?l-id=top_normal_function04&fidomy=1",
+    "purchase_order": {
+        "parent": "html > body > div:nth-of-type(1) > div:nth-of-type(7) > div:nth-of-type(2) > div:nth-of-type(1) > div > div:nth-of-type(3) > div > table > tbody > tr",
+        "order_number": {
+            "element": "td:nth-of-type(1) > div > ul:nth-of-type(1) > li:nth-of-type(2) > span.idNum",
+            "full_path": false,
+            "attribute": "",
+            "regex": ""
+        },
+        "order_date": {
+            "element": "td:nth-of-type(1) > div > ul:nth-of-type(1) > li.purchaseDate",
+            "full_path": false,
+            "attribute": "",
+            "regex": ""
+        },
+        "purchase_product": {
+            "url_element": "td:nth-of-type(1) > div > ul:nth-of-type(1) > li:nth-of-type(4) > a.detail",
+            "parent": "html > body > div:nth-of-type(1) > div:nth-of-type(7) > div:nth-of-type(2) > div > div:nth-of-type(3) > table:nth-of-type(1) > tbody > tr",
+            "total_amount": {
+                "element": "html > body > div:nth-of-type(1) > div:nth-of-type(7) > div:nth-of-type(2) > div > div:nth-of-type(3) > table:nth-of-type(2) > tbody > tr > td:nth-of-type(3) > table > tbody > tr:nth-of-type(4) > td.netTot",
+                "full_path": true,
+                "attribute": "",
+                "regex": ""
+            },
+            "product_code": {
+                "element": "td > table > tbody > tr > td:nth-of-type(2) > a.itemLink",
+                "full_path": false,
+                "attribute": "href",
+                "regex": ".+item.rakuten.co.jp/([^/]+/[^/]+)/.*"
+            },
+            "product_name": {
+                "element": "td > table > tbody > tr > td:nth-of-type(2) > a.itemLink",
+                "full_path": false,
+                "attribute": "",
+                "regex": ""
+            },
+            "product_quantity": {
+                "element": "td.widthQuantity.taRight",
+                "full_path": false,
+                "attribute": "",
+                "regex": ""
+            },
+            "unit_price": {
+                "element": "td.widthPrice.taRight",
+                "full_path": false,
+                "attribute": "",
+                "regex": ""
+            },
+            "product_distributor": {
+                "element": "html > body > div:nth-of-type(1) > div:nth-of-type(7) > div:nth-of-type(2) > div > div:nth-of-type(1) > table > tbody > tr:nth-of-type(1) > td:nth-of-type(2) > p > a",
+                "full_path": true,
+                "attribute": "",
+                "regex": ""
             }
+       }
+    },
+    "next_url_element": "#oDrCenterContents .clfx:nth-child(11) [data-ratid=\\\"ph_pc_pagi_next\\\"]"
+}{
+     "url": "https://order.my.rakuten.co.jp/?l-id=top_normal_function04&fidomy=1",
+     "purchase_order": {
+         "parent": "html > body > div:nth-of-type(1) > div:nth-of-type(7) > div:nth-of-type(2) > div:nth-of-type(1) > div > div:nth-of-type(3) > div > table > tbody > tr",
+         "order_number": {
+             "element": "td:nth-of-type(1) > div > ul:nth-of-type(1) > li:nth-of-type(2) > span.idNum",
+             "full_path": false,
+             "attribute": "",
+             "regex": ""
+         },
+         "order_date": {
+             "element": "td:nth-of-type(1) > div > ul:nth-of-type(1) > li.purchaseDate",
+             "full_path": false,
+             "attribute": "",
+             "regex": ""
+         },
+         "purchase_product": {
+             "url_element": "td:nth-of-type(1) > div > ul:nth-of-type(1) > li:nth-of-type(4) > a.detail",
+             "parent": "html > body > div:nth-of-type(1) > div:nth-of-type(7) > div:nth-of-type(2) > div > div:nth-of-type(3) > table:nth-of-type(1) > tbody > tr",
+             "total_amount": {
+                 "element": "html > body > div:nth-of-type(1) > div:nth-of-type(7) > div:nth-of-type(2) > div > div:nth-of-type(3) > table:nth-of-type(2) > tbody > tr > td:nth-of-type(3) > table > tbody > tr:nth-of-type(4) > td.netTot",
+                 "full_path": true,
+                 "attribute": "",
+                 "regex": ""
+             },
+             "product_code": {
+                 "element": "td > table > tbody > tr > td:nth-of-type(2) > a.itemLink",
+                 "full_path": false,
+                 "attribute": "href",
+                 "regex": ".+item.rakuten.co.jp/([^/]+/[^/]+)/.*"
+             },
+             "product_name": {
+                 "element": "td > table > tbody > tr > td:nth-of-type(2) > a.itemLink",
+                 "full_path": false,
+                 "attribute": "",
+                 "regex": ""
+             },
+             "product_quantity": {
+                 "element": "td.widthQuantity.taRight",
+                 "full_path": false,
+                 "attribute": "",
+                 "regex": ""
+             },
+             "unit_price": {
+                 "element": "td.widthPrice.taRight",
+                 "full_path": false,
+                 "attribute": "",
+                 "regex": ""
+             },
+             "product_distributor": {
+                 "element": "html > body > div:nth-of-type(1) > div:nth-of-type(7) > div:nth-of-type(2) > div > div:nth-of-type(1) > table > tbody > tr:nth-of-type(1) > td:nth-of-type(2) > p > a",
+                 "full_path": true,
+                 "attribute": "",
+                 "regex": ""
+             }
         }
-    }
+     },
+     "next_url_element": "#oDrCenterContents .clfx:nth-child(11) [data-ratid=\\\"ph_pc_pagi_next\\\"]"
+ }
+ ',
+@yahoo_purchase_history_script = '
+{
+    "url": "https://odhistory.shopping.yahoo.co.jp/cgi-bin/history-list?sc_i=shp_pc_my_MHD_order_history",
+    "purchase_order": {
+        "parent": "html > body > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div:nth-of-type(1) > div:nth-of-type(2) > ul > li",
+        "order_number": {
+            "element": "div:nth-of-type(2) > ul > li > ul:nth-of-type(1) > li:nth-of-type(2) > dl > dd",
+            "full_path": false,
+            "attribute": "",
+            "regex": ""
+        },
+        "order_date": {
+            "element": "div:nth-of-type(1) > p:nth-of-type(1) > span",
+            "full_path": false,
+            "attribute": "",
+            "regex": ""
+        },
+        "purchase_product": {
+            "url_element": "div:nth-of-type(2) > ul > li > ul:nth-of-type(3) > li:nth-of-type(1) > a",
+            "parent": "html > body > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div:nth-of-type(3) > div:nth-of-type(2) > ul > li",
+            "total_amount": {
+                "element": "html > body > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div:nth-of-type(2) > div > div:nth-of-type(2) > div:nth-of-type(1) > div:nth-of-type(2) > ul > li.elSum > dl > dd",
+                "full_path": true,
+                "attribute": "",
+                "regex": ""
+            },
+            "product_code": {
+                "element": "div > ul > li:nth-of-type(2) > p > a",
+                "full_path": false,
+                "attribute": "href",
+                "regex": "https://.*pagekey=(.*)"
+            },
+            "product_name": {
+                "element": "div > dl > dd:nth-of-type(1) > a > span",
+                "full_path": false,
+                "attribute": "",
+                "regex": ""
+            },
+            "product_quantity": {
+                "element": "div > dl > dd > span:nth-of-type(2).elNum",
+                "full_path": false,
+                "attribute": "",
+                "regex": ""
+            },
+            "unit_price": {
+                "element": "div > dl > dd > span:nth-of-type(1).elPrice",
+                "full_path": false,
+                "attribute": "",
+                "regex": ""
+            },
+            "product_distributor": {
+                "element": "html > body > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div > div:nth-of-type(2) > ul:nth-of-type(1) > li:nth-of-type(1) > a",
+                "full_path": true,
+                "attribute": "",
+                "regex": ""
+            }
+       }
+    },
+    "next_url_element": "html > body > div:nth-of-type(1) > div > div:nth-of-type(2) > div > div:nth-of-type(1) > div > div:nth-of-type(2) > div > ul > li:nth-of-type(2) > a"
 }
 ';
 

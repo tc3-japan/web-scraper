3cf0e3e7d8858f218a86e57b2e73aa0f1280ae15
shidayama.ew <shidayama@elastic-works.com>
2020-12-15 15:03:21 +0900

    Created login check batch. Added two columns to the ec_site_account table.

---
+++ web-scraper-server/src/main/java/com/topcoder/scraper/exception/CheckLoginException.java
@@ -0,0 +1,11 @@
+package com.topcoder.scraper.exception;
+
+public class CheckLoginException extends RuntimeException {
+
+    public CheckLoginException() {
+    }
+
+    public CheckLoginException(String message) {
+        super(message);
+    }
+}
---
+++ web-scraper-server/src/main/java/com/topcoder/scraper/module/ecunifiedmodule/GeneralLoginCheckModule.java
@@ -0,0 +1,97 @@
+package com.topcoder.scraper.module.ecunifiedmodule;
+
+import com.topcoder.common.dao.ECSiteAccountDAO;
+import com.topcoder.common.dao.UserDAO;
+import com.topcoder.common.repository.ConfigurationRepository;
+import com.topcoder.common.repository.ECSiteAccountRepository;
+import com.topcoder.common.repository.UserRepository;
+import com.topcoder.common.traffic.TrafficWebClient;
+import com.topcoder.common.util.Common;
+import com.topcoder.scraper.module.ILoginCheckModule;
+
+import com.topcoder.scraper.module.ecunifiedmodule.crawler.GeneralPurchaseHistoryCrawler;
+import com.topcoder.scraper.service.WebpageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.util.*;
+
+/**
+ *
+ */
+@Component
+public class GeneralLoginCheckModule implements ILoginCheckModule {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GeneralLoginCheckModule.class);
+
+    private final WebpageService webpageService;
+    private final ECSiteAccountRepository ecSiteAccountRepository;
+    private final UserRepository userRepository;
+    private GeneralPurchaseHistoryCrawler crawler;
+
+    @Autowired
+    ConfigurationRepository configurationRepository;
+
+    @Autowired
+    public GeneralLoginCheckModule(ECSiteAccountRepository ecSiteAccountRepository, WebpageService webpageService, UserRepository userRepository) {
+        this.ecSiteAccountRepository = ecSiteAccountRepository;
+        this.webpageService = webpageService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public String getModuleType() { return "general"; }
+
+    @Override
+    public void checkLogin() {
+        LOGGER.debug("[checkLogin] in");
+
+        Iterable<UserDAO> userDAOS = userRepository.findAll();
+        for (UserDAO userDAO : userDAOS) {
+            Iterable<ECSiteAccountDAO> accountDAOS = ecSiteAccountRepository.findAllByUserId(userDAO.getId());
+            if (accountDAOS == null) { continue; }
+
+            for (ECSiteAccountDAO ecSiteAccountDAO : accountDAOS) {
+                boolean isLogin = this.fetchPurchaseHistoryListForECSiteAccount(ecSiteAccountDAO);
+                if (isLogin) {
+                    ecSiteAccountDAO.setIsLogin(true);
+                    ecSiteAccountDAO.setLastLoginedAt(Date.from(Instant.now()));
+                    ecSiteAccountDAO.setUpdateAt(Date.from(Instant.now()));
+                } else {
+                    ecSiteAccountDAO.setIsLogin(false);
+                    ecSiteAccountDAO.setUpdateAt(Date.from(Instant.now()));
+                }
+                ecSiteAccountRepository.save(ecSiteAccountDAO);
+            }
+        }
+    }
+
+    private boolean fetchPurchaseHistoryListForECSiteAccount(ECSiteAccountDAO ecSiteAccountDAO) {
+        if (ecSiteAccountDAO.getEcUseFlag() != Boolean.TRUE) {
+            LOGGER.info("EC Site [" + ecSiteAccountDAO.getId() + ":" + ecSiteAccountDAO.getEcSite() + "] is not active. Skipped.");
+            return false;
+        }
+        this.crawler = new GeneralPurchaseHistoryCrawler(ecSiteAccountDAO.getEcSite(), webpageService, this.configurationRepository);
+
+        TrafficWebClient webClient = new TrafficWebClient(ecSiteAccountDAO.getUserId(), true);
+        boolean restoreRet = Common.restoreCookies(webClient.getWebClient(), ecSiteAccountDAO);
+        if (!restoreRet) {
+            LOGGER.error("skip ec site account id = " + ecSiteAccountDAO.getId() + ", restore cookies failed");
+            return false;
+        }
+
+        try {
+            this.crawler.goToPurchaseHistoryListForLoginCheck(webClient);
+            webClient.finishTraffic();
+            return true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            LOGGER.error("failed to Login for ec site account id = " + ecSiteAccountDAO.getId());
+            return false;
+        }
+    }
+}

8c40c6b2c31fd04eadb4a3e5ad9d85ec925c028e
Tetsuo Shioda <shioda@tc3.co.jp>
2021-01-14 19:58:45 +0900

    Fixed Order Filter selector

--- web-scraper-server/src/main/resources/db/migration/V90__init-data.sql
+++ web-scraper-server/src/main/resources/db/migration/V90__init-data.sql
@@ -26,7 +26,7 @@ SET
 {
   "url": "https://www.amazon.co.jp/gp/your-account/order-history?orderFilter=year-{year}",
   "order_filter": {
-    "element": "html > body > div:nth-of-type(1) > div > div:nth-of-type(1) > div > div:nth-of-type(2) > span > form > span:nth-of-type(1) > select > option",
+    "element": "html > body > div > div > div:nth-of-type(1) > div > div:nth-of-type(2) > span > form > span:nth-of-type(1) > select > option",
     "attribute": "value",
     "regex": "year-([0-9]{4})"
   },
